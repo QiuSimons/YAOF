25dbd1f84cd42480179614b67f91b4c23a217def
QiuSimons <QiuSimons@users.noreply.github.com>
2020-07-17 11:23:01 +0800

    kernel config: CONFIG_ROCKCHIP_THERMAL=y

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -4,5 +4,6 @@ cd openwrt
 git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
 git remote add upstream https://github.com/openwrt/openwrt.git && git fetch upstream
 git rebase upstream/master
+sed -i "s/# CONFIG_ROCKCHIP_THERMAL is not set/CONFIG_ROCKCHIP_THERMAL=y/g" target/linux/rockchip/armv8/config-5.4
 cd ..
 exit 0
