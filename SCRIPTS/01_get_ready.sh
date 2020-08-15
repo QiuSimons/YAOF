7d2d0b74a47fab3a5a38f817dc2f12344a10b7e0
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-08-15 18:04:25 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -4,6 +4,11 @@ cd openwrt
 git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
 git remote add upstream https://github.com/openwrt/openwrt.git && git fetch upstream
 git rebase upstream/master
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
 cd ..
 git clone https://github.com/Lienol/openwrt.git openwrt-lienol
 exit 0
