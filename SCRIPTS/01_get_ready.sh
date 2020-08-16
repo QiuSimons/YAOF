88734472424d3e4530860cd72fe71947ada1a7bf
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-08-16 13:01:25 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-git clone -b master https://git.openwrt.org/openwrt/staging/blocktrron.git openwrt
-cd openwrt
-git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
-git remote add upstream https://github.com/openwrt/openwrt.git && git fetch upstream
-git rebase upstream/master
-git rebase --skip
-git rebase --skip
-git rebase --skip
-git rebase --skip
-git rebase --skip
-cd ..
+#git clone -b master https://git.openwrt.org/openwrt/staging/blocktrron.git openwrt
+#cd openwrt
+#git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
+#git remote add upstream https://github.com/openwrt/openwrt.git && git fetch upstream
+#git rebase upstream/master
+#git rebase --skip
+#git rebase --skip
+#git rebase --skip
+#git rebase --skip
+#git rebase --skip
+#cd ..
+git clone -b master https://git.openwrt.org/openwrt/openwrt.git openwrt
 git clone https://github.com/Lienol/openwrt.git openwrt-lienol
 exit 0
