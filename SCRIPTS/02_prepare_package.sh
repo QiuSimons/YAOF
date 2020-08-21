bfb486b3af85c62511c9c5a8715a2e4071e35c56
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-08-21 12:00:11 +0800

    Revert: crypto: test crypto patch made by @AmadeusGhost

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -6,7 +6,7 @@ clear
 wget -O- https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3277.patch | patch -p1
 
 #Crypto（test
-wget -O- https://github.com/AmadeusGhost/lede/commit/3e668936669080ca6f3fcea5534b94d00103291a.patch | patch -p1
+#wget -O- https://github.com/AmadeusGhost/lede/commit/3e668936669080ca6f3fcea5534b94d00103291a.patch | patch -p1
 
 ##准备工作
 #使用19.07的feed源
