a72bf73bfd8a38b120d5316113d4fa4c65ac1fab
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-08-27 11:07:34 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -5,7 +5,7 @@ clear
 wget -O- https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3277.patch | patch -p1
 
 #HW-RNG
-patch -p1 < ../PATCH/new/main/Support-hardware-random-number-generator-for-RK3328.patch
+git apply ../PATCH/new/main/Support-hardware-random-number-generator-for-RK3328.patch
 
 #Crypto（test
 #wget -O- https://github.com/AmadeusGhost/lede/commit/3e668936669080ca6f3fcea5534b94d00103291a.patch | patch -p1
