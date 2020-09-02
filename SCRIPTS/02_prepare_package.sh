418f2635d3ae0c995a613701786260064e1943a1
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-09-02 18:04:49 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -6,9 +6,16 @@ wget -O- https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3277.
 
 #RT Kernel
 cp -f ../PATCH/new/main/999-patch-5.4.61-rt37.patch ./target/linux/generic/hack-5.4/999-patch-5.4.61-rt37.patch
+sed -i '/PREEMPT/d' ./target/linux/rockchip/armv8/config-5.4
 echo '
-CONFIG_PREEMPT_RT_FULL=y
+CONFIG_PREEMPT_RT=y
+CONFIG_PREEMPTION=y
 ' >> ./target/linux/rockchip/armv8/config-5.4
+sed -i '/PREEMPT/d' ./target/linux/rockchip/config-default
+echo '
+CONFIG_PREEMPT_RT=y
+CONFIG_PREEMPTION=y
+' >> ./target/linux/rockchip/config-default
 
 #HW-RNG
 patch -p1 < ../PATCH/new/main/Support-hardware-random-number-generator-for-RK3328.patch
