5c9740873c326e3ef22ef11ade90e50fd434931c
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-11-04 22:23:57 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#notExce(){ 
+notExce(){ 
 git clone -b master https://git.openwrt.org/openwrt/staging/blocktrron.git openwrt
 cd openwrt
 git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
@@ -11,8 +11,8 @@ git rebase --skip
 git rebase --skip
 git rebase --skip
 cd ..
-#}
-#git clone -b master https://git.openwrt.org/openwrt/openwrt.git openwrt
+}
+git clone -b master https://git.openwrt.org/openwrt/openwrt.git openwrt
 git clone https://github.com/Lienol/openwrt.git openwrt-lienol
 git clone https://github.com/Lienol/openwrt-packages packages-lienol
 git clone https://github.com/Lienol/openwrt-luci luci-lienol
