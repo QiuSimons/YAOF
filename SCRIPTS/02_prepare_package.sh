cf56e9f09d2058fa06b490e58a61e87456a847e0
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-11-17 10:23:34 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -255,7 +255,6 @@ sed -i 's,include ../..,include $(TOPDIR)/feeds/luci,g' ./package/new/luci-app-s
 #上网APP过滤
 git clone -b master --single-branch https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter
 #Docker
-sed -i "s,libblkid,libblkid +libselinux +libsepol,g" packages/utils/lvm2/Makefile
 svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman package/luci-app-dockerman
 svn co https://github.com/lisaac/luci-lib-docker/trunk/collections/luci-lib-docker package/luci-lib-docker
 svn co https://github.com/openwrt/packages/trunk/utils/docker-ce feeds/packages/utils/docker-ce
@@ -272,6 +271,8 @@ svn co https://github.com/openwrt/packages/trunk/utils/runc feeds/packages/utils
 ln -sf ../../../feeds/packages/utils/runc ./package/feeds/packages/runc
 svn co https://github.com/openwrt/packages/trunk/utils/yq feeds/packages/utils/yq
 ln -sf ../../../feeds/packages/utils/yq ./package/feeds/packages/yq
+rm -rf ./feeds/packages/utils/lvm2
+svn co https://github.com/openwrt/packages/trunk/utils/lvm2 feeds/packages/utils/lvm2
 #补全部分依赖（实际上并不会用到
 svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/utils/fuse package/utils/fuse
 svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/network/services/samba36 package/network/services/samba36
