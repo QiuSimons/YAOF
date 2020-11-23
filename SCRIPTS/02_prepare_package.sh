afe6805c459b410bd111cbc02c04be192a738f1f
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-11-24 00:36:27 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -50,8 +50,8 @@ patch -p1 < ../PATCH/new/main/0001-tools-add-upx-ucl-support.patch
 sed -i 's,SNAPSHOT,,g' include/version.mk
 sed -i 's,snapshots,,g' package/base-files/image-config.in
 #使用O3级别的优化
-sed -i 's/Os/O2 -fno-reorder-blocks -fno-tree-ch/g' include/target.mk
-sed -i 's/O2/O2 -fno-reorder-blocks -fno-tree-ch/g' ./rules.mk
+sed -i 's/Os/O2 -fno-reorder-blocks -fno-tree-ch -fno-caller-saves/g' include/target.mk
+sed -i 's/O2/O2 -fno-reorder-blocks -fno-tree-ch -fno-caller-saves/g' ./rules.mk
 #更新feed
 ./scripts/feeds update -a && ./scripts/feeds install -a
 #RNGD
