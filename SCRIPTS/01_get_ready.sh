016e2ee60f57af1f711e15ce0ee7a41bc956cb94
QiuSimons <QiuSimons@users.noreply.github.com>
2020-11-28 12:22:22 +0800

    Revert "Update 01_get_ready.sh"
    This reverts commit 108c7e2410a20d3f7a0eefcb1b5351bc96577cdc.

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone -b master https://git.openwrt.org/openwrt/openwrt.git openwrt
-git clone https://github.com/QiuSimons/openwrt-lienol.git openwrt-lienol
-git clone https://github.com/QiuSimons/packages-lienol.git packages-lienol
-git clone https://github.com/QiuSimons/luci-lienol.git luci-lienol
-git clone -b linksys-ea6350v3-mastertrack https://github.com/QiuSimons/openwrt-NoTengoBattery.git NoTengoBattery
+git clone https://github.com/Lienol/openwrt.git openwrt-lienol
+git clone https://github.com/Lienol/openwrt-packages packages-lienol
+git clone https://github.com/Lienol/openwrt-luci luci-lienol
+git clone -b linksys-ea6350v3-mastertrack https://github.com/NoTengoBattery/openwrt NoTengoBattery
 exit 0
