21977d8f5afc28bfac22faea90195cfa437dbba0
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-11-28 21:44:50 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -139,7 +139,8 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-accessco
 cp -rf ../PATCH/duplicate/luci-app-control-weburl ./package/new/luci-app-control-weburl
 #AutoCore
 svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/autocore package/lean/autocore
-svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/coremark package/lean/coremark
+svn co https://github.com/project-openwrt/packages/trunk/utils/coremark feeds/packages/utils/coremark
+ln -sf ../../../feeds/packages/utils/coremark ./package/feeds/packages/coremark
 #迅雷快鸟
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-xlnetacc package/lean/luci-app-xlnetacc
 git clone https://github.com/garypang13/luci-app-xlnetacc package/lean/luci-app-xlnetacc
