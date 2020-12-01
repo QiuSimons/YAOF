4ce1d27230afc1563aa237ec62105e2d6abc048f
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-12-01 20:49:12 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone -b master --depth 1 https://git.openwrt.org/openwrt/openwrt.git openwrt
+git clone -b master https://git.openwrt.org/openwrt/openwrt.git openwrt
 git clone -b main --depth 1 https://github.com/Lienol/openwrt.git openwrt-lienol
 git clone -b main --depth 1 https://github.com/Lienol/openwrt-packages packages-lienol
 git clone -b main --depth 1 https://github.com/Lienol/openwrt-luci luci-lienol
