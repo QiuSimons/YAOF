2c8bc55626b69a9adbff1b1002eaa4cc79f6e8e8
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-12-01 16:50:30 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-git clone -b master https://git.openwrt.org/openwrt/openwrt.git openwrt
-git clone -b main https://github.com/Lienol/openwrt.git openwrt-lienol
-git clone -b main https://github.com/Lienol/openwrt-packages packages-lienol
-git clone -b main https://github.com/Lienol/openwrt-luci luci-lienol
-git clone -b linksys-ea6350v3-mastertrack https://github.com/NoTengoBattery/openwrt NoTengoBattery
+git clone -b master --depth 1 https://git.openwrt.org/openwrt/openwrt.git openwrt
+git clone -b main --depth 1 https://github.com/Lienol/openwrt.git openwrt-lienol
+git clone -b main --depth 1 https://github.com/Lienol/openwrt-packages packages-lienol
+git clone -b main --depth 1 https://github.com/Lienol/openwrt-luci luci-lienol
+git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
 exit 0
