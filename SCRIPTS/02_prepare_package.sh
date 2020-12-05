cfecf49425ccc961180ed440da443cb93f26e0fe
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-12-05 10:21:38 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -189,7 +189,9 @@ sed -i '/V2ray:v2ray/d' package/lean/luci-app-vssr/Makefile
 #SSRP
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/lean/luci-app-ssr-plus
 pushd package/lean
-wget -O- https://patch-diff.githubusercontent.com/raw/fw876/helloworld/pull/250.patch | git apply
+wget -qO - https://patch-diff.githubusercontent.com/raw/fw876/helloworld/pull/234.patch | patch -p1
+wget -qO - https://patch-diff.githubusercontent.com/raw/fw876/helloworld/pull/250.patch | patch -p1
+wget -qO - https://patch-diff.githubusercontent.com/raw/fw876/helloworld/pull/253.patch | patch -p1
 popd
 sed -i 's,default n,default y,g' package/lean/luci-app-ssr-plus/Makefile
 sed -i '/V2ray:v2ray/d' package/lean/luci-app-ssr-plus/Makefile
