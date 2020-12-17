0f88ec8e3384cfa5ae431a22530d8ceafa1d7905
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-12-17 11:10:30 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -104,7 +104,7 @@ rm -rf ./package/kernel/cryptodev-linux
 svn co https://github.com/project-openwrt/openwrt/trunk/package/kernel/cryptodev-linux package/kernel/cryptodev-linux
 #降级openssl（解决性能问题
 rm -rf ./package/libs/openssl
-svn co https://github.com/openwrt/openwrt/branches/efe837de842bf4c9df1deae22e4bdaab3f92d960/package/libs/openssl package/libs/openssl
+svn co -r 90110 https://github.com/openwrt/openwrt/trunk/package/libs/openssl package/libs/openssl
 #更换lzo
 svn co https://github.com/openwrt/packages/trunk/libs/lzo feeds/packages/libs/lzo
 ln -sf ../../../feeds/packages/libs/lzo ./package/feeds/packages/lzo
