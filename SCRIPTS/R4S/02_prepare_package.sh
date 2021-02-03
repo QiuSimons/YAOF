d9609a0bc64b9447190b7228fafea4c97317d82a
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-02-04 00:07:10 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/R4S/02_prepare_package.sh
+++ SCRIPTS/R4S/02_prepare_package.sh
@@ -271,8 +271,7 @@ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/new
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/new/brook
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/new/trojan-plus
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/new/ssocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray package/new/xray
-sed -i 's,default n,default y,g' package/new/xray/Makefile
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-core package/new/xray-core
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray package/new/v2ray
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-plugin package/new/v2ray-plugin
 #luci-app-cpulimit
