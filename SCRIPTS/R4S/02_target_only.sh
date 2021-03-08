2faf879817c3c40e433d1d5316622c8396eb8a30
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-03-08 22:16:46 +0800

    Update 02_target_only.sh

--- SCRIPTS/R4S/02_target_only.sh
+++ SCRIPTS/R4S/02_target_only.sh
@@ -14,7 +14,7 @@ cp -f ../PATCH/new/main/991-rockchip-rk3399-overclock-to-2.2-1.8-GHz-for-NanoPi4
 cp -f ../PATCH/new/main/213-RK3399-set-critical-CPU-temperature-for-thermal-throttling.patch ./target/linux/rockchip/patches-5.4/213-RK3399-set-critical-CPU-temperature-for-thermal-throttling.patch
 
 #使用特定的优化
-sed -i 's,-mcpu=generic,-mabi=lp64 -march=armv8-a+crypto+crc,g' include/target.mk
+sed -i 's,-mcpu=generic,-march=armv8-a+crypto+crc -mabi=lp64,g' include/target.mk
 cp -f ../PATCH/new/package/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch ./package/libs/mbedtls/patches/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch
 
 #Experimental
