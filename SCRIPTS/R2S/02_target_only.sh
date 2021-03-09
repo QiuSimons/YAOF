b54c9575d80b6e88ffade8e90f0b1015eb9855e1
jorejia <lijia12@lzu.edu.cn>
2021-03-10 01:26:02 +0800

    Update 02_target_only.sh

--- SCRIPTS/R2S/02_target_only.sh
+++ SCRIPTS/R2S/02_target_only.sh
@@ -20,6 +20,8 @@ sed -i '/;;/i\ethtool -K eth0 rx off tx off && logger -t disable-offloading "dis
 #SWAP LAN WAN（满足千兆场景，可选
 sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #翻译及部分功能优化
 cp -rf ../PATCH/duplicate/addition-trans-zh-r2s ./package/lean/lean-translate
 
