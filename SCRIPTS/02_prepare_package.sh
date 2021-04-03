b82256eefac58c5c01cb4fb85bff8c54feb0b318
super27035 <43180500+super27035@users.noreply.github.com>
2021-04-04 00:50:03 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -297,6 +297,12 @@ bash move_2_services.sh
 popd
 rm -rf ./feeds/packages/net/zerotier/files/etc/init.d/zerotier
 
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#2 password
+sed -i 's/root::0:0:99999:7:::/root:$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/base-files/files/etc/shadow
+#3. Modify default hosename
+sed -i 's/OpenWrt/SUPERouter/g' package/base-files/files/bin/config_generate
 
 ### 最后的收尾工作 ###
 # Lets Fuck
