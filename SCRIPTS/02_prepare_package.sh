08ba8b6762a485a56eb6cab8e3137272fc18f314
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-05-07 20:44:31 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -256,7 +256,7 @@ svn co https://github.com/immortalwrt/packages/trunk/net/shadowsocks-rust packag
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/lean/luci-app-ssr-plus
 rm -rf ./package/lean/luci-app-ssr-plus/po/zh_Hans
 pushd package/lean
-wget -qO - https://github.com/fw876/helloworld/pull/509.patch | patch -p1
+#wget -qO - https://github.com/fw876/helloworld/pull/509.patch | patch -p1
 wget -qO - https://github.com/QiuSimons/helloworld-fw876/commit/c1674ad.patch | patch -p1
 popd
 pushd package/lean/luci-app-ssr-plus
