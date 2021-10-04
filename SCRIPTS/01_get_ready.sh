75dc2b58fabee46480432b099819e50324630bc9
zming66 <50068931+zming66@users.noreply.github.com>
2021-10-05 01:23:11 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -15,10 +15,10 @@ mkdir openwrt
 cp -rf ./openwrt_new/* ./openwrt/
 
 # 获取源代码
-#git clone --single-branch -b openwrt-21.02 https://github.com/openwrt/openwrt openwrt
-#git clone -b main --depth 1 https://github.com/Lienol/openwrt.git openwrt-lienol
-#git clone -b main --depth 1 https://github.com/Lienol/openwrt-packages packages-lienol
-#git clone -b main --depth 1 https://github.com/Lienol/openwrt-luci luci-lienol
-#git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
+git clone --single-branch -b openwrt-21.02 https://github.com/openwrt/openwrt openwrt
+git clone -b main --depth 1 https://github.com/Lienol/openwrt.git openwrt-lienol
+git clone -b main --depth 1 https://github.com/Lienol/openwrt-packages packages-lienol
+git clone -b main --depth 1 https://github.com/Lienol/openwrt-luci luci-lienol
+git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
 
 exit 0
