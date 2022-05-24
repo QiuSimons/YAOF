51eee508b283f1617305d91aadae592bc693a4a7
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2022-05-24 22:29:39 +0800

    misc: use fw4 & nftables (#420)
    * nft test branch
    
    * add missing kconfig

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -122,17 +122,8 @@ svn export https://github.com/coolsnowwolf/lede/trunk/package/boot/uboot-rockchi
 svn export https://github.com/immortalwrt/immortalwrt/branches/master/package/boot/arm-trusted-firmware-rockchip-vendor package/boot/arm-trusted-firmware-rockchip-vendor
 rm -rf ./package/kernel/linux/modules/video.mk
 wget -P package/kernel/linux/modules/ https://github.com/immortalwrt/immortalwrt/raw/master/package/kernel/linux/modules/video.mk
-rm -rf ./target/linux/generic/config-5.10
-wget -P target/linux/generic/ https://github.com/immortalwrt/immortalwrt/raw/master/target/linux/generic/config-5.10
-echo '
-CONFIG_DEBUG_INFO_REDUCED=y
-' >>./target/linux/generic/config-5.10
 # LRNG
 cp -rf ../PATCH/LRNG/* ./target/linux/generic/hack-5.10/
-echo '
-CONFIG_LRNG=y
-CONFIG_LRNG_JENT=y
-' >>./target/linux/generic/config-5.10
 # ImmortalWrt Uboot TMP Fix
 #wget -qO- https://github.com/immortalwrt/immortalwrt/commit/433c93e.patch | patch -REp1
 wget -qO- https://github.com/coolsnowwolf/lede/commit/0104258.patch | patch -REtp1
@@ -313,7 +304,7 @@ svn export https://github.com/QiuSimons/dragino2-teasiu/trunk/package/teasiu/luc
 #svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-passwall package/new/luci-app-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/new/luci-app-passwall
 pushd package/new/luci-app-passwall
-#sed -i 's,iptables-legacy,iptables-nft,g' Makefile
+sed -i 's,iptables-legacy,iptables-nft,g' Makefile
 popd
 wget -P package/new/luci-app-passwall/ https://github.com/QiuSimons/OpenWrt-Add/raw/master/move_2_services.sh
 chmod -R 755 ./package/new/luci-app-passwall/move_2_services.sh
@@ -352,7 +343,7 @@ pushd package/new/luci-app-passwall2
 bash move_2_services.sh
 popd
 pushd package/new/luci-app-passwall2
-#sed -i 's,iptables-legacy,iptables-nft,g' Makefile
+sed -i 's,iptables-legacy,iptables-nft,g' Makefile
 popd
 # qBittorrent 下载
 svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-qbittorrent package/lean/luci-app-qbittorrent
@@ -477,7 +468,7 @@ rm -rf ./feeds/packages/net/zerotier/files/etc/init.d/zerotier
 #sed -i '/Default,one/a\\t$(STAGING_DIR_HOST)/bin/upx --lzma --best $(PKG_BUILD_DIR)/zerotier-one' feeds/packages/net/zerotier/Makefile
 # 翻译及部分功能优化
 svn export https://github.com/QiuSimons/OpenWrt-Add/trunk/addition-trans-zh package/lean/lean-translate
-#sed -i 's,iptables-mod-fullconenat,iptables-nft +kmod-nft-fullcone,g' package/lean/lean-translate/Makefile
+sed -i 's,iptables-mod-fullconenat,iptables-nft +kmod-nft-fullcone,g' package/lean/lean-translate/Makefile
 
 ### 最后的收尾工作 ###
 # Lets Fuck
@@ -488,6 +479,51 @@ wget -P package/base-files/files/usr/bin/ https://github.com/QiuSimons/OpenWrt-A
 sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 # 生成默认配置及缓存
 rm -rf .config
+
+echo '
+CONFIG_RESERVE_ACTIVEFILE_TO_PREVENT_DISK_THRASHING=y
+CONFIG_RESERVE_ACTIVEFILE_KBYTES=65536
+CONFIG_RESERVE_INACTIVEFILE_TO_PREVENT_DISK_THRASHING=y
+CONFIG_RESERVE_INACTIVEFILE_KBYTES=65536
+
+CONFIG_LRNG=y
+# CONFIG_LRNG_OVERSAMPLE_ENTROPY_SOURCES is not set
+CONFIG_LRNG_IRQ=y
+CONFIG_LRNG_CONTINUOUS_COMPRESSION_ENABLED=y
+# CONFIG_LRNG_CONTINUOUS_COMPRESSION_DISABLED is not set
+CONFIG_LRNG_ENABLE_CONTINUOUS_COMPRESSION=y
+# CONFIG_LRNG_SWITCHABLE_CONTINUOUS_COMPRESSION is not set
+# CONFIG_LRNG_COLLECTION_SIZE_32 is not set
+# CONFIG_LRNG_COLLECTION_SIZE_256 is not set
+# CONFIG_LRNG_COLLECTION_SIZE_512 is not set
+CONFIG_LRNG_COLLECTION_SIZE_1024=y
+# CONFIG_LRNG_COLLECTION_SIZE_2048 is not set
+# CONFIG_LRNG_COLLECTION_SIZE_4096 is not set
+# CONFIG_LRNG_COLLECTION_SIZE_8192 is not set
+CONFIG_LRNG_COLLECTION_SIZE=1024
+# CONFIG_LRNG_HEALTH_TESTS is not set
+CONFIG_LRNG_IRQ_ENTROPY_RATE=256
+# CONFIG_LRNG_JENT is not set
+CONFIG_LRNG_CPU=y
+CONFIG_LRNG_CPU_ENTROPY_RATE=8
+# CONFIG_LRNG_DRNG_SWITCH is not set
+# CONFIG_LRNG_TESTING_MENU is not set
+# CONFIG_LRNG_SELFTEST is not set
+
+# CONFIG_IR_SANYO_DECODER is not set
+# CONFIG_IR_SHARP_DECODER is not set
+# CONFIG_IR_MCE_KBD_DECODER is not set
+# CONFIG_IR_XMP_DECODER is not set
+# CONFIG_IR_IMON_DECODER is not set
+# CONFIG_IR_RCMM_DECODER is not set
+# CONFIG_IR_SPI is not set
+# CONFIG_IR_GPIO_TX is not set
+# CONFIG_IR_PWM_TX is not set
+# CONFIG_IR_SERIAL is not set
+# CONFIG_IR_SIR is not set
+# CONFIG_RC_XBOX_DVD is not set
+# CONFIG_IR_TOY is not set
+' >>./target/linux/generic/config-5.10
 
 ### Shortcut-FE 部分 ###
 # Patch Kernel 以支持 Shortcut-FE
