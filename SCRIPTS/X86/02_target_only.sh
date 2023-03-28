1337c570df1a8ed44ba75a53d2bad60640497381
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2023-03-28 13:42:48 +0800

    WIP

--- SCRIPTS/X86/02_target_only.sh
+++ SCRIPTS/X86/02_target_only.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i 's/O2 -Wl,--gc-sections/O2 -Wl,--gc-sections -mtune=goldmont-plus/g' include/target.mk
+sed -i 's/O2/O2 -mtune=goldmont-plus/g' include/target.mk
 
 rm -rf ./package/kernel/linux/modules/video.mk
 cp -rf ../lede/package/kernel/linux/modules/video.mk ./package/kernel/linux/modules/video.mk
