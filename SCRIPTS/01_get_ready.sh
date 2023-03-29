4c2b2a22abfc9b3b077af022d2e5d5e7ec6cef6c
sonic0377 <54339314+sonic0377@users.noreply.github.com>
2023-03-29 20:40:57 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,54 +1,37 @@
 #!/bin/bash
 
-latest_release="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][0-9]/p' | sed -n 1p | sed 's/.tar.gz//g')"
-git clone --single-branch -b ${latest_release} https://github.com/openwrt/openwrt openwrt_release
-git clone --single-branch -b openwrt-22.03 https://github.com/openwrt/openwrt openwrt
-rm -f ./openwrt/include/version.mk
-rm -f ./openwrt/include/kernel.mk
-rm -f ./openwrt/include/kernel-5.10
-rm -f ./openwrt/include/kernel-version.mk
-rm -f ./openwrt/include/toolchain-build.mk
-rm -f ./openwrt/include/kernel-defaults.mk
-rm -f ./openwrt/package/base-files/image-config.in
-rm -rf ./openwrt/target/linux/*
-rm -rf ./openwrt/package/kernel/linux/*
-cp -f ./openwrt_release/include/version.mk ./openwrt/include/version.mk
-cp -f ./openwrt_release/include/kernel.mk ./openwrt/include/kernel.mk
-cp -f ./openwrt_release/include/kernel-5.10 ./openwrt/include/kernel-5.10
-cp -f ./openwrt_release/include/kernel-version.mk ./openwrt/include/kernel-version.mk
-cp -f ./openwrt_release/include/toolchain-build.mk ./openwrt/include/toolchain-build.mk
-cp -f ./openwrt_release/include/kernel-defaults.mk ./openwrt/include/kernel-defaults.mk
-cp -f ./openwrt_release/package/base-files/image-config.in ./openwrt/package/base-files/image-config.in
-cp -f ./openwrt_release/version ./openwrt/version
-cp -f ./openwrt_release/version.date ./openwrt/version.date
-cp -rf ./openwrt_release/target/linux/* ./openwrt/target/linux/
-cp -rf ./openwrt_release/package/kernel/linux/* ./openwrt/package/kernel/linux/
+sed -i 's/O2/O2 -mtune=goldmont-plus/g' include/target.mk
 
-# 获取源代码
-git clone -b master --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt
-git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt_21
-git clone -b master --depth 1 https://github.com/immortalwrt/packages.git immortalwrt_pkg
-git clone -b master --depth 1 https://github.com/immortalwrt/luci.git immortalwrt_luci
-git clone -b master --depth 1 https://github.com/coolsnowwolf/lede.git lede
-git clone -b master --depth 1 https://github.com/coolsnowwolf/luci.git lede_luci
-git clone -b master --depth 1 https://github.com/coolsnowwolf/packages.git lede_pkg
-git clone -b master --depth 1 https://github.com/openwrt/openwrt.git openwrt_ma
-git clone -b master --depth 1 https://github.com/openwrt/packages.git openwrt_pkg_ma
-git clone -b master --depth 1 https://github.com/openwrt/luci.git openwrt_luci_ma
-git clone -b master --depth 1 https://github.com/Lienol/openwrt.git Lienol
-git clone -b main --depth 1 https://github.com/Lienol/openwrt-package Lienol_pkg
-git clone -b master --depth 1 https://github.com/QiuSimons/OpenWrt-Add.git OpenWrt-Add
-git clone -b master --depth 1 https://github.com/nxhack/openwrt-node-packages.git openwrt-node
-git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall passwall_pkg
-git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall passwall_luci
-git clone -b main --depth 1 https://github.com/jjm2473/openwrt-third openwrt-third
-git clone -b master --depth 1 https://github.com/lisaac/luci-app-dockerman dockerman
-git clone -b master --depth 1 https://github.com/lisaac/luci-app-diskman diskman
-git clone -b master --depth 1 https://github.com/lisaac/luci-lib-docker docker_lib
-git clone -b master --depth 1 https://github.com/QiuSimons/openwrt-mos mosdns
-git clone -b master --depth 1 https://github.com/fw876/helloworld ssrp
-git clone -b master --depth 1 https://github.com/zxlhhyccc/bf-package-master zxlhhyccc
-git clone -b main --depth 1 https://github.com/linkease/openwrt-app-actions linkease
-#git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
+rm -rf ./package/kernel/linux/modules/video.mk
+cp -rf ../lede/package/kernel/linux/modules/video.mk ./package/kernel/linux/modules/video.mk
+sed -i '/nouveau\.ko/d' package/kernel/linux/modules/video.mk
+sed -i 's,CONFIG_DRM_I915_CAPTURE_ERROR ,CONFIG_DRM_I915_CAPTURE_ERROR=n ,g' package/kernel/linux/modules/video.mk
+
+echo '# Put your custom commands here that should be executed once
+# the system init finished. By default this file does nothing.
+
+grep "Default string" /tmp/sysinfo/model >> /dev/null
+if [ $? -ne 0 ];then
+    echo should be fine
+else
+    echo "Compatible PC" > /tmp/sysinfo/model
+fi
+
+exit 0
+'> ./package/base-files/files/etc/rc.local
+
+
+#Vermagic
+latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][0-9]/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
+wget https://downloads.openwrt.org/releases/${latest_version}/targets/x86/64/packages/Packages.gz
+zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' >.vermagic
+sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+
+# 预配置一些插件
+cp -rf ../PATCH/files ./files
+
+chmod -R 755 ./
+find ./ -name *.orig | xargs rm -f
+find ./ -name *.rej | xargs rm -f
 
 exit 0
