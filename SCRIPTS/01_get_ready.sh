6ff3fdeda3ae9fd20ca7504d4e31859134476a6a
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2023-05-23 00:20:36 +0800

    use imm 21 upnp

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -28,7 +28,9 @@ git clone --single-branch -b openwrt-23.05 https://github.com/openwrt/openwrt op
 git clone -b master --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt
 git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt_21
 git clone -b master --depth 1 https://github.com/immortalwrt/packages.git immortalwrt_pkg
+git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/packages.git immortalwrt_pkg_21
 git clone -b master --depth 1 https://github.com/immortalwrt/luci.git immortalwrt_luci
+git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/luci.git immortalwrt_luci_21
 git clone -b master --depth 1 https://github.com/coolsnowwolf/lede.git lede
 git clone -b master --depth 1 https://github.com/coolsnowwolf/luci.git lede_luci
 git clone -b master --depth 1 https://github.com/coolsnowwolf/packages.git lede_pkg
