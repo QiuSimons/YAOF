54e9fcb7dd9431f3979b4e9dfbac8b536b802856
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2023-08-03 18:01:07 +0800

    sfe: try fix udp timeout

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -473,6 +473,7 @@ cp -rf ../lede/package/lean/shortcut-fe/fast-classifier ./package/lean/shortcut-
 wget -qO - https://github.com/coolsnowwolf/lede/commit/331f04f.patch | patch -p1
 wget -qO - https://github.com/coolsnowwolf/lede/commit/232b8b4.patch | patch -p1
 wget -qO - https://github.com/coolsnowwolf/lede/commit/ec795c9.patch | patch -p1
+wget -qO - https://github.com/coolsnowwolf/lede/commit/789f805.patch | patch -p1
 cp -rf ../lede/package/lean/shortcut-fe/shortcut-fe ./package/lean/shortcut-fe/shortcut-fe
 wget -qO - https://github.com/coolsnowwolf/lede/commit/e517080.patch | patch -p1
 wget -qO - https://github.com/coolsnowwolf/lede/commit/413cd91.patch | patch -p1
