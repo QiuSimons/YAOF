d9e321e15dd60d6d50b40381d17b0036edd90a4e
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2023-08-05 19:10:15 +0800

    refine script

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,45 +1,88 @@
 #!/bin/bash
 
+# 这个脚本的作用是从不同的仓库中克隆openwrt相关的代码，并进行一些处理
+
+# 定义一个函数，用来克隆指定的仓库和分支
+clone_repo() {
+  # 参数1是仓库地址，参数2是分支名，参数3是目标目录
+  repo_url=$1
+  branch_name=$2
+  target_dir=$3
+  # 克隆仓库到目标目录，并指定分支名和深度为1
+  git clone -b $branch_name --depth 1 $repo_url $target_dir
+}
+
+# 定义一些变量，存储仓库地址和分支名
 latest_release="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][3-9]/p' | sed -n 1p | sed 's/.tar.gz//g')"
-git clone --single-branch -b ${latest_release} https://github.com/openwrt/openwrt openwrt
-git clone --single-branch -b openwrt-23.05 https://github.com/openwrt/openwrt openwrt_snap
+immortalwrt_repo="https://github.com/immortalwrt/immortalwrt.git"
+immortalwrt_pkg_repo="https://github.com/immortalwrt/packages.git"
+immortalwrt_luci_repo="https://github.com/immortalwrt/luci.git"
+lede_repo="https://github.com/coolsnowwolf/lede.git"
+lede_luci_repo="https://github.com/coolsnowwolf/luci.git"
+lede_pkg_repo="https://github.com/coolsnowwolf/packages.git"
+openwrt_repo="https://github.com/openwrt/openwrt.git"
+openwrt_pkg_repo="https://github.com/openwrt/packages.git"
+openwrt_luci_repo="https://github.com/openwrt/luci.git"
+lienol_repo="https://github.com/Lienol/openwrt.git"
+lienol_pkg_repo="https://github.com/Lienol/openwrt-package"
+openwrt_add_repo="https://github.com/QiuSimons/OpenWrt-Add.git"
+openwrt_node_repo="https://github.com/nxhack/openwrt-node-packages.git"
+passwall_pkg_repo="https://github.com/xiaorouji/openwrt-passwall"
+passwall_luci_repo="https://github.com/xiaorouji/openwrt-passwall"
+openwrt_third_repo="https://github.com/jjm2473/openwrt-third"
+dockerman_repo="https://github.com/lisaac/luci-app-dockerman"
+diskman_repo="https://github.com/lisaac/luci-app-diskman"
+docker_lib_repo="https://github.com/lisaac/luci-lib-docker"
+mosdns_repo="https://github.com/QiuSimons/openwrt-mos"
+ssrp_repo="https://github.com/fw876/helloworld"
+zxlhhyccc_repo="https://github.com/zxlhhyccc/bf-package-master"
+linkease_repo="https://github.com/linkease/openwrt-app-actions"
+linkease_pkg_repo="https://github.com/jjm2473/packages"
+linkease_luci_repo="https://github.com/jjm2473/luci"
+
+# 开始克隆仓库，并行执行
+clone_repo $openwrt_repo $latest_release openwrt &
+clone_repo $openwrt_repo openwrt-23.05 openwrt_snap &
+clone_repo $immortalwrt_repo master immortalwrt &
+clone_repo $immortalwrt_repo openwrt-21.02 immortalwrt_21 &
+clone_repo $immortalwrt_repo openwrt-23.05 immortalwrt_23 &
+clone_repo $immortalwrt_pkg_repo master immortalwrt_pkg &
+clone_repo $immortalwrt_pkg_repo openwrt-21.02 immortalwrt_pkg_21 &
+clone_repo $immortalwrt_luci_repo master immortalwrt_luci &
+clone_repo $immortalwrt_luci_repo openwrt-21.02 immortalwrt_luci_21 &
+clone_repo $immortalwrt_luci_repo openwrt-23.05 immortalwrt_luci_23 &
+clone_repo $lede_repo master lede &
+clone_repo $lede_luci_repo master lede_luci &
+clone_repo $lede_pkg_repo master lede_pkg &
+clone_repo $openwrt_repo main openwrt_ma &
+clone_repo $openwrt_repo openwrt-22.03 openwrt_22 &
+clone_repo $openwrt_pkg_repo master openwrt_pkg_ma &
+clone_repo $openwrt_luci_repo master openwrt_luci_ma &
+clone_repo $lienol_repo master Lienol &
+clone_repo $lienol_pkg_repo main Lienol_pkg &
+clone_repo $openwrt_add_repo master OpenWrt-Add &
+clone_repo $openwrt_node_repo master openwrt-node &
+clone_repo $passwall_pkg_repo packages passwall_pkg &
+clone_repo $passwall_luci_repo luci passwall_luci &
+clone_repo $openwrt_third_repo main openwrt-third &
+clone_repo $dockerman_repo master dockerman &
+clone_repo $diskman_repo master diskman &
+clone_repo $docker_lib_repo master docker_lib &
+clone_repo $mosdns_repo master mosdns &
+clone_repo $ssrp_repo master ssrp &
+clone_repo $zxlhhyccc_repo main zxlhhyccc &
+clone_repo $linkease_repo main linkease &
+clone_repo $linkease_pkg_repo istoreos-21.02 linkease_pkg &
+clone_repo $linkease_luci_repo istoreos-21.02 linkease_luci &
+
+# 等待所有后台任务完成
+wait
+
+# 进行一些处理
 find openwrt/package/* -maxdepth 0 ! -name 'firmware' ! -name 'kernel' ! -name 'base-files' ! -name 'Makefile' -exec rm -rf {} +
 rm -rf ./openwrt_snap/package/firmware ./openwrt_snap/package/kernel ./openwrt_snap/package/base-files ./openwrt_snap/package/Makefile
 cp -rf ./openwrt_snap/package/* ./openwrt/package/
 cp -rf ./openwrt_snap/feeds.conf.default ./openwrt/feeds.conf.default
 
-# 获取源代码
-git clone -b master --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt
-git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt_21
-git clone -b openwrt-23.05 --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt_23
-git clone -b master --depth 1 https://github.com/immortalwrt/packages.git immortalwrt_pkg
-git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/packages.git immortalwrt_pkg_21
-git clone -b master --depth 1 https://github.com/immortalwrt/luci.git immortalwrt_luci
-git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/luci.git immortalwrt_luci_21
-git clone -b openwrt-23.05 --depth 1 https://github.com/immortalwrt/luci.git immortalwrt_luci_23
-git clone -b master --depth 1 https://github.com/coolsnowwolf/lede.git lede
-git clone -b master --depth 1 https://github.com/coolsnowwolf/luci.git lede_luci
-git clone -b master --depth 1 https://github.com/coolsnowwolf/packages.git lede_pkg
-git clone -b main --depth 1 https://github.com/openwrt/openwrt.git openwrt_ma
-git clone -b openwrt-22.03 --depth 1 https://github.com/openwrt/openwrt.git openwrt_22
-git clone -b master --depth 1 https://github.com/openwrt/packages.git openwrt_pkg_ma
-git clone -b master --depth 1 https://github.com/openwrt/luci.git openwrt_luci_ma
-git clone -b master --depth 1 https://github.com/Lienol/openwrt.git Lienol
-git clone -b main --depth 1 https://github.com/Lienol/openwrt-package Lienol_pkg
-git clone -b master --depth 1 https://github.com/QiuSimons/OpenWrt-Add.git OpenWrt-Add
-git clone -b master --depth 1 https://github.com/nxhack/openwrt-node-packages.git openwrt-node
-git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall passwall_pkg
-git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall passwall_luci
-git clone -b main --depth 1 https://github.com/jjm2473/openwrt-third openwrt-third
-git clone -b master --depth 1 https://github.com/lisaac/luci-app-dockerman dockerman
-git clone -b master --depth 1 https://github.com/lisaac/luci-app-diskman diskman
-git clone -b master --depth 1 https://github.com/lisaac/luci-lib-docker docker_lib
-git clone -b master --depth 1 https://github.com/QiuSimons/openwrt-mos mosdns
-git clone -b master --depth 1 https://github.com/fw876/helloworld ssrp
-git clone -b master --depth 1 https://github.com/zxlhhyccc/bf-package-master zxlhhyccc
-git clone -b main --depth 1 https://github.com/linkease/openwrt-app-actions linkease
-git clone -b istoreos-21.02 --depth 1 https://github.com/jjm2473/packages linkease_pkg
-git clone -b istoreos-21.02 --depth 1 https://github.com/jjm2473/luci linkease_luci
-#git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
-
+# 退出脚本
 exit 0
