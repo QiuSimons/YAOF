70a40ffba3723fb535b6d3008b09a0c64292c9a5
Liu Kai <103993324@qq.com>
2024-08-27 16:04:52 +0000

    modified:   SCRIPTS/09_diy.sh

--- SCRIPTS/09_diy.sh
+++ SCRIPTS/09_diy.sh
@@ -2,3 +2,5 @@
 
 #1. 修改默认IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' feeds/luci/collections/luci/Makefile
