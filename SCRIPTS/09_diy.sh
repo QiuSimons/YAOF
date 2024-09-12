1c7cc2d3b23eeb77c41a48abc5233eac83cf4c85
Liu Kai <103993324@qq.com>
2024-09-12 17:37:26 +0800

    modified:   .github/workflows/X86-OpenWrt.yml 	modified:   SCRIPTS/09_diy.sh 	modified:   SEED/X86/config.seed

--- SCRIPTS/09_diy.sh
+++ SCRIPTS/09_diy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #1. 修改默认IP
-sed -i 's/192.168.1.1/192.168.10.1/g' Lienol/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 #2. 删除最后一行
-sed -i '/\/usr\/bin\/zsh/d' Lienol/package/base-files/files/files/etc/profile
+# sed -i '/\/usr\/bin\/zsh/d' package/base-files/files/files/etc/profile
 #3. 修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' Lienol/immortalwrt_23/package/feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' Lienol/immortalwrt_23/package/feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' Lienol/immortalwrt_23/package/feeds/luci/collections/luci-ssl-nginx/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' Lienol/immortalwrt_23/package/feeds/luci/collections/luci-light/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' package/feeds/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' package/feeds/luci/luci-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' package/feeds/luci/luci-ssl-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' package/feeds/luci/luci-light/Makefile
